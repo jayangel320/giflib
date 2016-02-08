8e2c7cb82249413285f9c39a1aba6c45f11c7bb8
Tommy Morgan <jangel1@bu.edu>
2016-02-08 15:39:07 -0500

    Added two models classes

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,17 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
+import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Created by jaybob320 on 2/5/16.
@@ -10,9 +19,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GifController {
 
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
-    public String listGifs(){
+    public String listGifs(ModelMap modelMap){
+    List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs",allGifs);
         return "home";
+    }
+
+
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = gifRepository.findByName(name);
+        //this is a map!
+        modelMap.put("gif",gif);
+        return "gif-details";
     }
 
 }
---
+++ src/main/java/com/teamtreehouse/giflib/model/Category.java
@@ -0,0 +1,31 @@
+package com.teamtreehouse.giflib.model;
+
+/**
+ * Created by jaybob320 on 2/8/16.
+ */
+public class Category {
+
+    private String name;
+    private int id;
+
+    public Category(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
